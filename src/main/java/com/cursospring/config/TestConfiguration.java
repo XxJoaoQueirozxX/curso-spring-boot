bf49287209118d08d9f4a924cb7d0b7bdbbb2db0
XxJoaoQueirozxX <joaovic.pqueiroz@gmail.com>
2021-03-15 12:48:31 -0300

    Multiplos perfis de configuracao

--- src/main/java/com/cursospring/config/TestConfiguration.java
+++ src/main/java/com/cursospring/config/TestConfiguration.java
@@ -1,9 +1,18 @@
 package com.cursospring.config;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class TestConfiguration {
 
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> {
+            System.out.println("Rodando a config de desenvolvimento");
+        };
+    }
 }
