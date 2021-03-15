e5b3f71c79de655600ca98d9d3d13fe33b13e341
XxJoaoQueirozxX <joaovic.pqueiroz@gmail.com>
2021-03-15 14:40:07 -0300

    delete e update de clientes com jdbc

--- src/main/java/com/cursospring/VendasApplication.java
+++ src/main/java/com/cursospring/VendasApplication.java
@@ -17,6 +17,8 @@ public class VendasApplication {
     @Bean
     public CommandLineRunner init(@Autowired ClienteRepository repository){
         return args -> {
+
+            System.out.println("Salvado ....");
             Cliente cliente = new Cliente();
             cliente.setNome("João");
             repository.save(cliente);
@@ -26,9 +28,37 @@ public class VendasApplication {
             repository.save(cliente);
 
 
+            System.out.println("Listando ...");
             List<Cliente> clientes = repository.findAll();
             clientes.forEach(System.out::println);
 
+
+            System.out.println("Atualizando ...");
+            clientes.forEach(c ->{
+                c.setNome(c.getNome() + "(Atualizado)");
+                repository.update(c);
+            });
+
+//            repository.delete(2L);
+
+            clientes = repository.findAll();
+            clientes.forEach(System.out::println);
+
+
+            System.out.println("Buscando por nome Car ...");
+            repository.findByName("Car").forEach(System.out::println);
+
+            System.out.println("Deletando clientes ....");
+            clientes.forEach(repository::delete);
+
+
+            clientes = repository.findAll();
+
+            if(clientes.isEmpty()){
+                System.out.println("Nenhum cliente encontrado");
+            }else{
+                clientes.forEach(System.out::println);
+            }
         };
     }
 
--- src/main/java/com/cursospring/repositories/ClienteRepository.java
+++ src/main/java/com/cursospring/repositories/ClienteRepository.java
@@ -13,26 +13,53 @@ import java.util.List;
 @Repository
 public class ClienteRepository {
 
-    private static String INSERT = "INSERT INTO CLIENTE (NOME) VALUES (?)";
-    private static String SELECT_ALL = "SELECT * FROM CLIENTE";
+    private static final String INSERT = "INSERT INTO CLIENTE (NOME) VALUES (?)";
+    private static final String SELECT_ALL = "SELECT * FROM CLIENTE";
+    private static final String UPDATE = "UPDATE CLIENTE SET NOME=? WHERE ID=?";
+    private static final String DELETE = "DELETE FROM CLIENTE WHERE ID=?";
+
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
 
     public Cliente save(Cliente cliente){
-        jdbcTemplate.update(INSERT, new Object[]{cliente.getNome()});
+        jdbcTemplate.update(INSERT, cliente.getNome());
         return cliente;
     }
 
     public List<Cliente> findAll(){
-        return jdbcTemplate.query(SELECT_ALL, new RowMapper<Cliente>(){
-            public Cliente mapRow(ResultSet rs, int i) throws SQLException{
+        return jdbcTemplate.query(SELECT_ALL, getRowMapper());
+    }
+
+    private RowMapper<Cliente> getRowMapper() {
+        return new RowMapper<Cliente>() {
+            public Cliente mapRow(ResultSet rs, int i) throws SQLException {
                 return new Cliente(
-                    rs.getLong("id"),
-                    rs.getString("nome")
+                        rs.getLong("id"),
+                        rs.getString("nome")
                 );
             }
-        });
+        };
     }
+
+    public List<Cliente> findByName(String name){
+        return jdbcTemplate.query(SELECT_ALL.concat(" WHERE NOME LIKE ?"), new String[]{"%"+name+"%"},  getRowMapper());
+    }
+
+
+    public Cliente update(Cliente c){
+        jdbcTemplate.update(UPDATE, c.getNome(), c.getId());
+        return c;
+    }
+
+    public void delete(Cliente c){
+        delete(c.getId());
+    }
+
+    public void delete(Long id){
+        jdbcTemplate.update(DELETE, id);
+    }
+
+
 }
