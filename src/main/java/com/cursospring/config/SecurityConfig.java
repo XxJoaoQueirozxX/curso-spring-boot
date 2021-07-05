4ca658bafd3bc49f7491c2c38d9563e572941673
Joao Queiroz <joaovic.pqueiroz@gmail.com>
2021-07-04 22:18:32 -0300

    Basic authorization

--- src/main/java/com/cursospring/config/SecurityConfig.java
+++ src/main/java/com/cursospring/config/SecurityConfig.java
@@ -41,7 +41,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/produtos/**")
                     .hasRole("ADMIN")
             .and()
-                .formLogin();
-
+                .httpBasic();
     }
 }
