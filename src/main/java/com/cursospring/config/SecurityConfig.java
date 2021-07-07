cd05544415e82a8339ca00602644fe0de08ed44d
Joao Queiroz <joaovic.pqueiroz@gmail.com>
2021-07-06 21:47:01 -0300

    Auth Basic

--- src/main/java/com/cursospring/config/SecurityConfig.java
+++ src/main/java/com/cursospring/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.cursospring.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -46,8 +47,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .hasAnyRole("USER", "ADMIN")
                 .antMatchers("/api/produtos/**")
                     .hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "/api/usuarios")
+                    .permitAll()
                 .antMatchers("/h2-console/**")
                     .permitAll()
+                .anyRequest().authenticated()
             .and()
                 .httpBasic();
     }
